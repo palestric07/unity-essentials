477c8e36919712ad283312ee32784473be5eca7a
palestric07 <musadiq.munir1038@gmail.com>
2026-05-14 21:41:50 +0500

    importance of data and veriables

--- Assets/scripts/weapon.cs
+++ Assets/scripts/weapon.cs
@@ -1,18 +1,22 @@
-using UnityEngine;
+ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic; 
-public class weapon : MonoBehaviour
+public class weapon: MonoBehaviour
 {
+    [SerializeField]
+    private int Rotationspeed=200;
+    [SerializeField]
+    private Vector3 rotationpoint= Vector3.zero;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.forward, 200 * Time.deltaTime);
+        float rotationamount=  Rotationspeed *Time.deltaTime;
+            transform.RotateAround(rotationpoint, Vector3.forward,rotationamount);
     }
 }
- 
